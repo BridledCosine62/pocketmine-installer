f82537f03161e187333c87d176a24242950b7b56
ItzB <71258475+BridledCosine62@users.noreply.github.com>
2021-02-17 15:23:28 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "this script must run as root user! or using sudo"
+echo "installing curl...."
+apt install curl -y
+echo "making directory for pmmp..."
+mkdir pmmp
+cd pmmp
+echo "installing pmmp"
+curl -sL https://get.pmmp.io | bash -s - -r 
+echo "Done! the pmmp directory is on "pmmp"
+ls
